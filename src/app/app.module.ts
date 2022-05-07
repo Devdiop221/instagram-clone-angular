ffe847824985874c47f50993b3e2347d3e600ead
Devdiop221 <diopmouhamed290@gmail.com>
2022-05-07 23:13:33 +0100

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeLineComponentComponent } from './time-line-component/time-line-component.component';
+import { TopMenuComponentComponent } from './top-menu-component/top-menu-component.component';
+import { InstaIpdComponent } from './insta-ipd/insta-ipd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeLineComponentComponent,
+    TopMenuComponentComponent,
+    InstaIpdComponent
   ],
   imports: [
     BrowserModule,
